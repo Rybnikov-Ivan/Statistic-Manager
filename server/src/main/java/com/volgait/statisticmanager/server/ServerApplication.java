529d65b42005db72ae066f065ce06729df40afc5
iva <rybnikovv12345@gmail.com>
2022-03-26 18:12:29 +0400

    added server part

---
+++ server/src/main/java/com/volgait/statisticmanager/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.volgait.statisticmanager.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
